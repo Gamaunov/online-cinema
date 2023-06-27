979feafd010af2b6006d426691b03e1df1c5bc47
denis <gamaunov1911@gmail.com>
2023-06-27 15:09:25 +0300

    AuthProvider & roles

--- frontend/app/provders/MainProvider.tsx
+++ frontend/app/provders/MainProvider.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
+import { store } from 'store/store'
 
-import Layout from '@/components/layout/Layout'
 
-import { store } from '../store/store'
+import ReduxToastr from '@/ui/redux-toastr/ReduxToastr'
 
-import ReduxToast from './ReduxToast'
+import { TypeComponentAuthFields } from '@/shared/types/auth.types'
 import HeadProvider from './HeadProvider/HeadProvider'
+import AuthProvider from './AuthProvider/AuthProvider'
+import Layout from '@/components/layout/Layout'
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -22,8 +24,10 @@ const MainProvider: FC<TypeComponentAuthFields> = ({ children, Component }) => {
 		<HeadProvider>
 			<Provider store={store}>
 				<QueryClientProvider client={queryClient}>
-					<ReduxToast />
-					<Layout>{children}</Layout>
+					<ReduxToastr />
+					<AuthProvider Component={Component}>
+						<Layout>{children}</Layout>
+					</AuthProvider>
 				</QueryClientProvider>
 			</Provider>
 		</HeadProvider>
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from 'next/app'
+
+import { TypeComponentAuthFields } from '@/shared/types/auth.types'
+
+import '../app/assets/styles/globals.scss'
 import MainProvider from 'provders/MainProvider'
 
-import '@/assets/styles/globals.scss'
+type TypeAppProps = AppProps & TypeComponentAuthFields
 
-export default function App({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: TypeAppProps) => {
 	return (
 		<MainProvider Component={Component}>
 			<Component {...pageProps} />
 		</MainProvider>
 	)
 }
+
+export default MyApp
